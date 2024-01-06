d54f264a59eb3091d3d161503cb90e3c01ac0bef
VetleViking <vetlelind@gmail.com>
2024-01-06 23:25:18 +0100

    continuation of previous work

--- public/js/test.js
+++ public/js/test.js
@@ -14,19 +14,86 @@ function startGame() {
 
     let html = document.createElement("div");
 
-    html.innerHTML = `<p>${currentGame.title}</p><p>${currentGame.description}</p><br>}`;  
+    html.innerHTML = `<p id="gameTitle">${currentGame.title}</p><p>${currentGame.description}</p>`;  
 
 
     description.innerHTML = html.innerHTML;
+
+    let timer = document.getElementById("timerTask");
+    let time = 60;
+    timer.innerHTML = time;
+    let interval = setInterval(() => {
+        time--;
+        timer.innerHTML = time;
+        if (time == 0) {
+            clearInterval(interval);
+            open({
+                title: "Tiden er ute!",
+                message: "Du fikk ikke til oppgaven.",
+                message2: "-1000 social credits :(.",
+                onOk: () => {
+                    console.log("ok");
+                },
+                onCancel: () => {
+                    console.log("cancel");
+                },
+
+            });
+        }
+    }, 1000);
 }
+
+document.getElementById("submitBtn").addEventListener("click", () => {
+    let currentGame = undefined;
+    for (let i = 0; i < Object.keys(games).length; i++) {
+        if (document.getElementById("gameTitle").innerHTML == games[Object.keys(games)[i]].title) {
+            currentGame = games[Object.keys(games)[i]];
+            break;
+        }
+    }
+    if (currentGame.name == "game1") {
+        let iframe = document.getElementById("result");
+        
+        div1 = iframe.contentWindow.document.querySelector(".div1");
+        div2 = iframe.contentWindow.document.querySelector(".div2");
+
+        if (window.getComputedStyle(div1).backgroundColor == "rgb(255, 0, 0)" && window.getComputedStyle(div2).backgroundColor == "rgb(0, 0, 255)") {
+            open({
+                title: "Riktig!",
+                message: "Du klarte det!",
+                message2: "Du får 100 social credits :).",
+                onOk: () => {
+                    console.log("ok");
+                },
+                onCancel: () => {
+                    console.log("cancel");
+                },
+
+            });
+        } else {
+            open({
+                title: "Feil!",
+                message: "Du klarte det ikke.",
+                message2: "-100 social credits :(",
+                onOk: () => {
+                    console.log("ok");
+                },
+                onCancel: () => {
+                    console.log("cancel");
+                },
+
+            });
+        }
+    }
+});
 
 games = {
     game1: {
         name: "game1",
         title: "Bakgrunnsfarger",
-        description: "Gjør bakgrunnsfargen på  til rød (red eller #ff0000 i css).",
+        description: "Gjør bakgrunnsfargen på div1 til rød (red eller #ff0000 i css), og bakgrunnsfargen på div2 til blue (blue eller #0000ff).",
         html: `
-        <div class="div1">Div med class div1</div>
+        <div class="div1" id="test">Div med class div1</div>
         <div class="div2">Div med class div2</div>`,
         css: `.div1 {
 
